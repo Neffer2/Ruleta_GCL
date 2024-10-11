43d7f50dfbf3633d9dea520dea8aaf5c9ee9c7c6
Neffer2 <n3f73r@gmail.com>
2024-10-11 14:32:09 -0500

    1. Correccion mayúsculas

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { Boot } from './scenes/boot.js';
 import { Preloader } from './scenes/preloader.js';
 import { Game } from './scenes/game.js';
-import { GameOver } from './scenes/gameover.js';
+import { GameOver } from './scenes/gameOver.js';
 
 const config = {
     type: Phaser.AUTO,
