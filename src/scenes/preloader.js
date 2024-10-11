9dd1d4c89758fc6209f8920b46d394c5b52e482d
Neffer2 <n3f73r@gmail.com>
2024-10-11 14:27:10 -0500

    first commit

---
+++ src/scenes/preloader.js
@@ -0,0 +1,35 @@
+export class Preloader extends Phaser.Scene {
+    constructor ()
+    {
+        super('Preloader');
+    }
+
+    preload ()
+    {
+        this.load.setPath('public/assets');
+        this.load.image('background', 'bg.jpg');
+        this.load.image('bg-pop', 'bg-pop.png');
+        this.load.image('bg-light', 'bg-light.png');
+        this.load.image('bgp-1', 'bgp-1.png');
+        this.load.image('bgp-2', 'bgp-2.png');
+        this.load.image('bgp-3', 'bgp-3.png');
+        this.load.image('p-1', 'p-1.png');
+        this.load.image('p-2', 'p-2.png');
+        this.load.image('p-3', 'p-3.png');
+        this.load.image('logo', 'logo.png');
+        this.load.image('puntero', 'puntero.png');
+        this.load.image('ruleta', 'ruleta.png');
+        this.load.image('base', 'base.png');
+        this.load.image('girarBtn', 'button.png');   
+        this.load.image('detenerBtn', 'button-stop.png');
+        this.load.spritesheet('luces', 'luces-spritesheet.png', { frameWidth: 653.5, frameHeight: 654 });
+
+        let rect2 = this.make.graphics().fillStyle(0xFFFFFF).fillRect(50, 50, 150, 25);
+        rect2.generateTexture('rectangle', 280, 25);
+    }
+
+    create ()
+    {
+        this.scene.start('Game');
+    } 
+}
